2e75a78f0755cbb11cde6809daf5d9102f7ad7ef
Tadej Borovšak <tadej.borovsak@xlab.si>
2019-06-14 11:36:45 +0200

    Implement state machine stages

--- app/models/manageiq/providers/redfish/physical_infra_manager/bios_update/state_machine.rb
+++ app/models/manageiq/providers/redfish/physical_infra_manager/bios_update/state_machine.rb
@@ -1,14 +1,93 @@
 module ManageIQ::Providers::Redfish::PhysicalInfraManager::BiosUpdate::StateMachine
   def start_bios_update
-    update_and_notify_parent(:message => msg('start bios update'))
+    update_and_notify_parent(:message => msg("start BIOS update"))
 
-    $redfish_log.info("BIOS update on physical server [#{source.id}] #{source.name}")
-    $redfish_log.info("BIOS update with options #{options}")
+    case power_state = source.power_state_now
+    when "poweringon"  then signal :poll_server_on_initial
+    when "on"          then signal :power_off_server
+    when "poweringoff" then signal :poll_server_off
+    when "off"         then signal :apply_bios_settings
+    else raise MiqException::MiqProvisionError, "Unexpected #{power_state}"
+    end
+  end
 
-    # The two lines above log:
-    # [----] I, [2019-06-10T14:45:36.697490 #41212:2b1f16e24f18]  INFO -- : Q-task_id([r2_physical_server_bios_update_task_2]) BIOS update on physical server [2] Dell G5 Computer System (23840jr9384j)
-    # [----] I, [2019-06-10T14:45:36.697795 #41212:2b1f16e24f18]  INFO -- : Q-task_id([r2_physical_server_bios_update_task_2]) BIOS update with options {:request_type=>"physical_server_bios_update", :src_ids=>["2"], :config=>"{\n\"BUREK\": \"PICA\"\n}", :delivered_on=>2019-06-10 12:45:32 UTC}
+  def poll_server_on_initial
+    if source.powered_on_now?
+      signal :power_off_server
+    else
+      requeue_phase
+    end
+  end
 
+  def power_off_server
+    update_and_notify_parent(:message => msg("stop server"))
+    source.power_down
+    signal :poll_server_off
+  end
+
+  def poll_server_off
+    if source.powered_off_now?
+      signal :apply_bios_settings
+    else
+      requeue_phase
+    end
+  end
+
+  def apply_bios_settings
+    update_and_notify_parent(:message => msg("apply BIOS settings"))
+
+    source.with_provider_object do |system|
+      phase_context[:time] = system.Bios["@Redfish.Settings"].Time
+      phase_context[:attributes] = system.Bios.Attributes.raw
+
+      # TODO(@tadeboro): This is seriously bad, since we are loding YAML from
+      # an untrusted source.
+      attributes = YAML.load(options[:config])
+
+      system.Bios["@Redfish.Settings"].SettingsObject.patch(payload: {
+        "@Redfish.SettingsApplyTime" => { "ApplyTime" => "OnReset" },
+        "Attributes"                 => attributes
+      })
+
+      # Settings Once and BiosSetup in once PATCH call breaks on Dell.
+      system.patch(:payload => {
+        "Boot" => {
+          "BootSourceOverrideEnabled" => "Once",
+          "BootSourceOverrideTarget"  => "Pxe"
+        }
+      })
+      system.patch(:payload => {
+        "Boot" => { "BootSourceOverrideTarget" => "BiosSetup" }
+      })
+    end
+    source.power_up
+
+    signal :poll_bios_settings_applied
+  end
+
+  def poll_bios_settings_applied
+    settings_applied = source.with_provider_object do |system|
+      system.Bios["@Redfish.Settings"].Time != phase_context[:time] ||
+        # TODO(@tadeboro): Implement logic for determining the end of the
+        # operation on systems that do not have Time field. This is currently
+        # tweaked for Dell servers (14G).
+        system.Bios["@Redfish.Settings"].SettingsObject.Attributes.raw.empty?
+    end
+
+    if settings_applied
+      signal :store_diff
+    else
+      requeue_phase
+    end
+  end
+
+  def store_diff
+    update_and_notify_parent(:message => msg("store BIOS settings diff"))
+    old_attributes = phase_context[:attributes]
+    new_attributes = source.with_provider_object { |s| s.Bios.Attributes.raw }
+    options[:diff] = new_attributes.each_with_object([]) do |(k, v), acc|
+      acc << [k, old_attributes[k], v] if v != old_attributes[k]
+    end
     signal :done_bios_update
   end
 end
